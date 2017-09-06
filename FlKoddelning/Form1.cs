a0815ae3a49c83572b016b8ee12ade234c4b8fe2
Emilia Lundgren <emlu1402@student.miun.se>
2017-09-06 13:13:10 +0200

    Händelse under knappen

--- FlKoddelning/Form1.cs
+++ FlKoddelning/Form1.cs
@@ -16,5 +16,10 @@ namespace FlKoddelning
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
